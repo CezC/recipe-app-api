6ef19cc730f359baf741da22f0b65aaa693dc49d
CezC <cezary7c7@gmail.com>
2023-09-20 22:00:29 +0300

    added empty wait_for_db.py command

---
+++ app/core/management/commands/wait_for_db.py
@@ -0,0 +1,16 @@
+# minimum code to create stub for our commands
+# if we run this command from djago it will probablhy run becasuse it has right structure
+# with pass
+
+
+"""
+Django scommand to wait for the database to be available.
+"""
+from dhango.core.management.base import BaseCommand
+
+
+class Command(BaseCommand):
+    """Django command to wait for database."""
+
+    def handle(self, *args, **options):
+        pass
